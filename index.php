0ea7f3bfe21bd5df532136da17cea7b68263633b
ian castillero <ian.castillero@utp.ac.pa>
2024-08-29 19:36:37 -0500

    reorganizacion

--- index.php
+++ index.php
@@ -1,29 +1,79 @@
 <?php
-// Definición de variables
-$nombre = "María";
-$edad = 30;
-$ciudad = "Madrid";
-
-// Definición de constante
-define("PROFESION", "Ingeniera");
-
-// Creación de mensaje usando diferentes métodos de concatenación e impresión
-$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
-$mensaje2 = "Vivo en $ciudad y soy " . PROFESION . ".";
-
-echo $mensaje1 . "<br>";
-print($mensaje2 . "<br>");
-
-printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $ciudad, PROFESION);
-
-echo "<br>Información de debugging:<br>";
-var_dump($nombre);
-echo "<br>";
-var_dump($edad);
-echo "<br>";
-var_dump($ciudad);
-echo "<br>";
-var_dump(PROFESION);
-echo "<br>";
+  if (!empty($_GET['q'])) {
+    switch ($_GET['q']) {
+      case 'info':
+        phpinfo(); 
+        exit;
+      break;
+    }
+  }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laragon</title>
 
+        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Karla';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .opt {
+                margin-top: 30px;
+            }
+
+            .opt a {
+              text-decoration: none;
+              font-size: 150%;
+            }
+            
+            a:hover {
+              color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title" title="Laragon">Laragon</div>
+     
+                <div class="info"><br />
+                      <?php print($_SERVER['SERVER_SOFTWARE']); ?><br />
+                      PHP version: <?php print phpversion(); ?>   <span><a title="phpinfo()" href="/?q=info">info</a></span><br />
+                      Document Root: <?php print ($_SERVER['DOCUMENT_ROOT']); ?><br />
+
+                </div>
+                <div class="opt">
+                  <div><a title="Getting Started" href="https://laragon.org/docs">Getting Started</a></div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
